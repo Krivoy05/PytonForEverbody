874ebcc15019181e5594bf50eb99831cf41c7179
Serhii Kryvoruchko <sergkrivoy@gmail.com>
2022-03-31 17:23:21 +0200

    Added exersice_5.py

---
+++ 4  function/exersice_5.py
@@ -0,0 +1,23 @@
+#material from https://books.trinket.io/pfe/04-functions.html
+"""
+Exercise 5: What will the following Python program print out?
+
+def fred():
+   print("Zap")
+
+def jane():
+   print("ABC")
+
+jane()
+fred()
+jane()
+"""
+def fred():
+   print("Zap")
+
+def jane():
+   print("ABC")
+
+jane()
+fred()
+jane()
