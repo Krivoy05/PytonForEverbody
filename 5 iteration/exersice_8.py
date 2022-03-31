97a90bd5df5501c17e6de65649b89ee7462a1ffb
Serhii Kryvoruchko <sergkrivoy@gmail.com>
2022-03-31 21:52:57 +0200

    Added exersice_8.py

---
+++ 5 iteration/exersice_8.py
@@ -0,0 +1,42 @@
+"""
+Exercise 1: Write a program which repeatedly reads numbers until the user enters "done".
+Once "done" is entered, print out the total, count, and average of the numbers.
+If the user enters anything other than a number, detect their mistake using try and
+ except and print an error message and skip to the next number.
+
+Enter a number: 4
+Enter a number: 5
+Enter a number: bad data
+Invalid input
+Enter a number: 7
+Enter a number: done
+16 3 5.333333333333333
+"""
+stored_list = []
+stored_string = ""
+
+while True:
+    inputed_data = input("Enter a number: ")
+    if inputed_data == "done":
+        print(stored_string)
+        break
+    try:
+        float(inputed_data)
+        stored_string += " " + inputed_data
+    except:
+        print("Bad data")
+
+
+#Solution with list append
+"""
+while True:
+    inputed_data = input("Enter a number: ")
+    if inputed_data == "done":
+        for value in stored_list:
+            print(value)
+        break
+    try:
+        stored_list.append(float(inputed_data))
+    except:
+        print("Bad data")
+"""
