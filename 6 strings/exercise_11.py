b6a69c35cff3d76d483a57360b35bc3cb741e919
Serhii Kryvoruchko <sergkrivoy@gmail.com>
2022-04-03 12:29:43 +0200

    Added to exercise_11.py parsing float until find first letter

--- 6 strings/exercise_11.py
+++ 6 strings/exercise_11.py
@@ -6,12 +6,25 @@ str = 'X-DSPAM-Confidence:0.8475'
 Use find and string slicing to extract the portion of the string after the colon character
  and then use the float function to convert the extracted string into a floating point number."""
 
-str = 'X-DSPAM-Confidence:0.84754544'
+str = 'X-DSPAM-Confidence:0.847llll54544'
 
 colon_character_position = str.find(":")
 str_number_from_str = str[colon_character_position + 1:]
-try :
+exeption_str = ""
+try:
     float_from_str = float(str_number_from_str)
     print(float_from_str)
 except:
-    print("Can't convert to float")
+    print("Can't convert to float to end of line, try to parse parsial")
+    symbol_counter = 0
+    for symbol in str_number_from_str:
+        if symbol.isnumeric():
+            exeption_str += symbol
+            symbol_counter += 1
+        else:
+            if symbol == '.':
+                exeption_str += symbol
+                symbol_counter += 1
+            else:
+                break
+    print("Parsed str is: "+exeption_str)
