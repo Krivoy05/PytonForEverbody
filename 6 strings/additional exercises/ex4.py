029ae884558f7690b15a84188c30b80972613362
Serhii Kryvoruchko <sergkrivoy@gmail.com>
2022-04-03 14:27:46 +0200

    Added additional exercise ex4.py to 6 String->Additional exercises def count symbols was optimised

--- 6 strings/additional exercises/ex4.py
+++ 6 strings/additional exercises/ex4.py
@@ -32,6 +32,18 @@ def count_symbols(str):
             counter += 1
     return counter
 
+
+
+def count_symbols_optimised(str):
+    counter = 0
+    symbols_set_to_search = "!@#$%^&*()_+=-_?.,<> "
+    for symbol in symbols_set_to_search:
+        for character in str:
+            if character == symbol:
+                counter+=1
+    return counter
+
+
 def count_digits(str):
     counter = 0
     for symbol in str:
@@ -45,5 +57,5 @@ print("Total counts of chars, digits, and symbols")
 
 print("Chars = " + str(count_chars(str1)))
 print("Digits = " + str(count_digits(str1)))
-print("Symbol = " + str(count_symbols(str1)))
+print("Symbol = " + str(count_symbols_optimised(str1)))
 
