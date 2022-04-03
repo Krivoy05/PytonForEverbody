9cd13467e9e6d2bc47cd63ed9a0162be04361a19
Serhii Kryvoruchko <sergkrivoy@gmail.com>
2022-04-03 14:14:41 +0200

    Added additional exercise ex4.py to 6 String->Additional exercises def count symbols need to be optimised

---
+++ 6 strings/additional exercises/ex4.py
@@ -0,0 +1,49 @@
+"""
+Count all letters, digits, and special symbols from a given string
+
+Given:
+
+str1 = "P@#yn26at^&i5ve"
+
+Expected Outcome:
+
+Total counts of chars, digits, and symbols
+
+Chars = 8
+Digits = 3
+Symbol = 4
+"""
+def count_chars(str):
+    counter = 0
+    for symbol in str:
+        if symbol.isalpha():
+            counter += 1
+    return counter
+
+#need to optimise,no idea how to find symbols
+def count_symbols(str):
+    counter = 0
+    for symbol in str:
+        if symbol.isdigit():
+           counter = counter
+        elif symbol.isalpha():
+            counter = counter
+        else:
+            counter += 1
+    return counter
+
+def count_digits(str):
+    counter = 0
+    for symbol in str:
+        if symbol.isdigit():
+            counter += 1
+    return counter
+
+str1 = "P@#yn26at^&i5ve"
+
+print("Total counts of chars, digits, and symbols")
+
+print("Chars = " + str(count_chars(str1)))
+print("Digits = " + str(count_digits(str1)))
+print("Symbol = " + str(count_symbols(str1)))
+
