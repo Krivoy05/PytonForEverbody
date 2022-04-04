9f70f225c982319fdd4bf693a3e7e4340d26f20d
Serhii Kryvoruchko <sergkrivoy@gmail.com>
2022-04-04 16:12:49 +0200

    Fixed bug exercise ex16.py, and add new solution 6 String->Additional exercises

--- 6 strings/additional exercises/ex16.py
+++ 6 strings/additional exercises/ex16.py
@@ -19,8 +19,11 @@ def find_alphanumerical(input_string):
     word_until_whitespace = ''
     for symbol in input_string:
         if symbol ==' ':
+            contains_alpha = False
             for sub_symvol in word_until_whitespace:
-                if sub_symvol.isdigit():
+                if sub_symvol.isalpha():
+                    contains_alpha = True
+                if sub_symvol.isdigit() and contains_alpha:
                     result.append(word_until_whitespace)
                     break
             word_until_whitespace = ''
@@ -28,5 +31,15 @@ def find_alphanumerical(input_string):
             word_until_whitespace += symbol
     return result
 
-str1 = "Emma25 is Data scientist50 and AI Expert"
-#print(find_alphanumerical(str1))
+def find_alphanumerical_split_test(input_string):
+    result = []
+    words = input_string.split()
+    for word in words:
+        if any(char.isalpha() for char in word) and any(char.isdigit() for char in word):
+            result.append(word)
+    return result
+
+
+str1 = "Emma25 is Data scientist50 and AI Expert 50a"
+print(find_alphanumerical(str1))
+print(find_alphanumerical_split_test(str1))
