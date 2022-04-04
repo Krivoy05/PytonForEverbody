3e963ee4244fef1875b7871de2e2722f2f9992d3
Serhii Kryvoruchko <sergkrivoy@gmail.com>
2022-04-04 14:53:52 +0200

    Added additional exercise ex13.py 6 String->Additional exercises

---
+++ 6 strings/additional exercises/ex14.py
@@ -0,0 +1,30 @@
+"""
+Remove special symbols / punctuation from a string
+
+Given:
+
+str1 = "/*Jon is @developer & musician"
+
+Expected Output:
+
+"Jon is developer musician"
+"""
+import string
+
+
+def filter_alpha_manualy (input_string):
+    result_str =""
+    for symbol in str1:
+        if symbol.isalpha() or symbol == " ":
+            result_str += symbol
+    return result_str
+
+def filter_alpha_translate(input_string):
+    result_str = ""
+    dictionary = input_string.maketrans("","",string.punctuation)
+    result_str = input_string.translate(dictionary)
+    return result_str
+
+str1 = "/*Jon is @developer & musician"
+print(filter_alpha_manualy(str1))
+print(filter_alpha_translate(str1))
