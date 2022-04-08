d1cb86d4bdd05a5bdc7c3c8944868f7a5d7d271f
sergkrivoy@gmail.com <Krivoy1091990!!>
2022-04-08 16:41:34 +0200

    Added concatination list function append_to_list.py

--- 7 list/append_to_list.py
+++ 7 list/append_to_list.py
@@ -12,19 +12,41 @@ Expected output:
 [10, 20, [300, 400, [5000, 6000, 7000], 500], 30, 40]
 
 """
-def apend_to_list(list1,value):
-    result = list()
+
+
+def apend_to_list(list1,value_after_to_add,adding_value):
+    i =0
     for val in list1:
-        if list_inside_list(val):
-            check_for_searched_value(value)
-### do to
-    return result
+        if islist(val):
+            #jesli tak parsujemy liste def
+            parsing_list(list1,value_after_to_add,adding_value)
+        else:
+            check_and_add_value(list1,val,value_after_to_add,adding_value,i)
+        i += 1
+    return True
 
-def check_for_searched_value(value):
+def check_and_add_value(list1,cheking_value,value_after_to_add,adding_value,index):
+    if cheking_value == value_after_to_add:
+        list1.insert(index+1,adding_value)
+
+def islist(list1):
+    return isinstance(list1,list)
+
+def parsing_list(list1,value_after_to_add,adding_value):
+    i = 0;
+    for value_from_list in list1:
+        test = islist(value_from_list)
+        if islist(value_from_list):
+            parsing_list(value_from_list,value_after_to_add,adding_value)
+        else:
+            check_and_add_value(list1, value_from_list, value_after_to_add, adding_value,i)
+        i += 1
+    return True
 
 
-def list_inside_list(value):
-    return any(x for x in value)
-
-list1 = [10, 20, [300, 400, [5000, 6000], 500], 30, 40]
-print(apend_to_list(list1,6000))
+list1 = [10, 20, [300, 400, [000,000, 8000], 500], 30, 6000,40]
+print("Before add")
+print(list1)
+print("After add")
+apend_to_list(list1,6000,7000)
+print(list1)
